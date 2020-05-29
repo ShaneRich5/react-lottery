d33725bcb4343bbf48533a7139cf4404c51e93bc
Shane Richards <shane.richards@fticonsulting.com>
2020-05-28 23:17:52 -0400

    fix: set the correct abi

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import loadWeb3 from './web3';
+// import loadWeb3 from './web3';
+import createContract from './lottery.service';
 
-function App() {
-  loadWeb3().then(web3 => web3.eth.getAccounts()).then(console.log);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+class App extends React.Component {
+  async componentDidMount() {
+    // const lottery = await createContract().then();
+    // const manager = await lottery.methods.manager().call();
+    // console.log(manager);
+
+    const lottery = await createContract();
+    const manager = await lottery.methods.manager().call();
+
+    // .then(console.log);
+    // .then(lottery => lottery.methods.manager().call())
+    // .then(console.log);
+    // const manager = await lottery.methods.manager().call();
+    console.log(manager);
+
+    console.log('hi')
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/lottery.service.ts
+++ src/lottery.service.ts
@@ -1,44 +1,8 @@
 import loadWeb3 from './web3';
 
 const address = '0xA8e691598e4c927D78a83e1C24FA4B64fab1dfB8';
-const abi: any = [
-    { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
-    {
-        inputs: [],
-        name: 'enter',
-        outputs: [],
-        stateMutability: 'payable',
-        type: 'function'
-    },
-    {
-        inputs: [],
-        name: 'getPlayers',
-        outputs: [Array],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [],
-        name: 'manager',
-        outputs: [Array],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [],
-        name: 'pickWinner',
-        outputs: [],
-        stateMutability: 'payable',
-        type: 'function'
-    },
-    {
-        inputs: [Array],
-        name: 'players',
-        outputs: [Array],
-        stateMutability: 'view',
-        type: 'function'
-    }
-];
+const abi: any = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "enter", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "getPlayers", "outputs": [{ "internalType": "address payable[]", "name": "", "type": "address[]" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "manager", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "pickWinner", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "internalType": "address payable", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }]
+
 
 const createContract = async () => {
 
@@ -46,4 +10,4 @@ const createContract = async () => {
     return new web3.eth.Contract(abi, address);
 }
 
-export default createContract();
+export default createContract;
