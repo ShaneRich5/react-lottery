52157e4d7fd78c76961221d08e0c32e1f5fa2e9f
Shane Richards <shane.richards@fticonsulting.com>
2020-05-29 00:58:25 -0400

    feat: added the ability to enter a lottery

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,47 @@ import './App.css';
 import loadWeb3 from './web3';
 import createContract from './lottery.service';
 import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+import { isArray } from 'util';
 
 class App extends React.Component<any, any> {
-  state = { manager: '', players: [], balance: '', value: '' };
+  state = { manager: '', players: [], balance: '', value: '', message: '' };
   web3?: Web3;
+  lottery?: Contract;
 
   async componentDidMount() {
-    const lottery = await createContract();
-    const manager = await lottery.methods.manager().call();
-    const players = await lottery.methods.getPlayers().call();
+    this.lottery = await createContract();
+    const manager = await this.lottery.methods.manager().call();
+    const players = await this.lottery.methods.getPlayers().call();
 
     this.web3 = await loadWeb3();
-    const balance = await this.web3.eth.getBalance(lottery.options.address);
+    const balance = await this.web3.eth.getBalance(this.lottery.options.address);
 
     this.setState({ manager, players, balance });
+  }
+
+  onClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const accounts = await this.web3?.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await this.lottery?.methods.pickWinner().send()
+  }
+
+  onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const accounts = await this.web3?.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    if (isArray(accounts) && accounts.length > 0) {
+      await this.lottery?.methods.enter().send({
+        from: accounts[0],
+        value: this.web3?.utils.toWei(this.state.value, 'ether')
+      });
+    }
+
+    this.setState({ 'message': 'You have been entered!' });
   }
 
   render() {
@@ -30,7 +57,7 @@ class App extends React.Component<any, any> {
         </p>
         <hr />
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
@@ -41,6 +68,10 @@ class App extends React.Component<any, any> {
           </div>
           <button>Enter</button>
         </form>
+
+        <hr />
+
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
